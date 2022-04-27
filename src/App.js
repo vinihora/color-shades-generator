a0e66025c349045dd43b095b9a2f997f2ed5ba1c
Vinicius Hora <vinidahora08@hotmail.com>
2022-04-27 01:23:33 -0300

    final commit, falta mudar a cor do hex da cor

--- src/App.js
+++ src/App.js
@@ -4,25 +4,54 @@ import React, { useState } from "react";
 
 const App = () => {
   const [color, setColor] = useState("#f15025");
-
-  let teste = new Values(color).all(10);
+  const [isError, setIsError] = useState(false);
+  const [teste, setTeste] = useState(new Values("#f15025").all(10));
 
   const handleChange = (e) => {
     setColor(e.target.value);
   };
 
-  console.log(teste);
+  const handleClick = () => {
+    const valor = document.getElementById("color").value;
+    console.log(valor);
+    try {
+      setTeste(new Values(valor).all(10));
+      setColor(valor);
+      console.log("cheguei");
+    } catch {
+      setIsError(true);
+    }
+  };
+
   return (
     <>
       <section className="header">
-        <h1>Color Generator</h1>
-        <input
-          type="text"
-          placeholder="#f15025"
-          id="color"
-          value={color}
-          onChange={handleChange}
-        />
+        <div className="title-header">
+          <h1>Color Generator</h1>
+        </div>
+        <div className="form">
+          <input
+            type="text"
+            placeholder="#f15025"
+            id="color"
+            value={color}
+            onChange={handleChange}
+          />
+          <button className="btn" onClick={() => handleClick()}>
+            <h3>Submit</h3>
+          </button>
+        </div>
+      </section>
+      <section className="colors">
+        {teste.map((color) => {
+          const cor = "#" + color.hex;
+          return (
+            <div className="color" style={{ background: cor }}>
+              <h4>{color.weight + "%"}</h4>
+              <h4>{"#" + color.hex}</h4>
+            </div>
+          );
+        })}
       </section>
     </>
   );
